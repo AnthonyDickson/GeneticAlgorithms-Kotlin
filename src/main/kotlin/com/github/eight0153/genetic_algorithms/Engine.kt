5eb47553208f72f4a36edc5f46b965e52c66fe8f
Anthony Dickson <eight0153@gmail.com>
2019-08-06 11:09:05 +1200

    make main loop synced

--- src/main/kotlin/com/github/eight0153/genetic_algorithms/Engine.kt
+++ src/main/kotlin/com/github/eight0153/genetic_algorithms/Engine.kt
@@ -7,21 +7,18 @@ import org.lwjgl.opengl.GL
 import org.lwjgl.opengl.GL11.*
 import org.lwjgl.system.MemoryUtil.NULL
 
-/** A RGBA colour. */
 data class Colour(val red: Float = 0f, val green: Float = 0f, val blue: Float = 0f, val alpha: Float = 1f)
-
-/** A 2D size. */
 data class Size(val width: Int = 0, val height: Int = 0)
 
 class Engine(
-    /** The name to appear on the window handle. */
     private val windowName: String,
     /** The width and height of the window in pixels. */
     private val windowSize: Size = Size(
         800,
         600
     ),
-    /** The colour of the background */
+    /** How often to update the display. For example, 60 fps is 1/60 seconds. */
+    private val targetFrameTime: Float = 1 / 60f,
     private val backgroundColour: Colour = Colour()
 ) {
 
@@ -48,8 +45,8 @@ class Engine(
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE)
 
-        // Create the window
         window = glfwCreateWindow(windowSize.width, windowSize.height, windowName, NULL, NULL)
+
         if (window == NULL) {
             throw RuntimeException("Failed to create the GLFW window")
         }
@@ -64,13 +61,13 @@ class Engine(
         })
 
         // Get the resolution of the primary monitor
-        val vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor())
+        val videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor())
 
         // Center our window
         glfwSetWindowPos(
             window!!,
-            (vidmode!!.width() - windowSize.width) / 2,
-            (vidmode.height() - windowSize.height) / 2
+            (videoMode!!.width() - windowSize.width) / 2,
+            (videoMode.height() - windowSize.height) / 2
         )
 
         // Make the OpenGL context current
@@ -82,10 +79,7 @@ class Engine(
         glfwShowWindow(window!!)
     }
 
-    /**
-     * The main loop.
-     */
-    private fun loop() {
+    private fun mainLoop() {
         // This line is critical for LWJGL's interoperation with GLFW's
         // OpenGL context, or any context that is managed externally.
         // LWJGL detects the context that is current in the current thread,
@@ -96,37 +90,73 @@ class Engine(
         // Set the clear color
         glClearColor(backgroundColour.red, backgroundColour.green, backgroundColour.blue, backgroundColour.alpha)
 
+        var previous = glfwGetTime()
+        var processingTime = 0.0
+
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
         while (!glfwWindowShouldClose(window!!)) {
+            val loopStartTime = glfwGetTime()
+            val elapsed = loopStartTime - previous
+            previous = loopStartTime
+            processingTime += elapsed
 
-            // Clear the framebuffer
-            glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
-
-            // Swap the color buffers
-            glfwSwapBuffers(window!!)
+            // TODO: Draw this via OpenGL rather than logging
+            println("Frame time: %.2f".format(1f / elapsed))
 
             // Poll for window events. The key callback above will only be
             // invoked during this call.
             glfwPollEvents()
+
+            // Update game state while we have time.
+            while (processingTime >= targetFrameTime) {
+                updateGameState()
+                processingTime -= targetFrameTime
+            }
+
+            render()
+            sync(loopStartTime)
         }
 
     }
 
+    private fun updateGameState() {}
+
+    /**
+     * Render the scene to the window.
+     */
+    private fun render() {
+        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+
+        glfwSwapBuffers(window!!)
+    }
+
+    /** Wait until [targetFrameTime] seconds have passed since [loopStartTime].*/
+    private fun sync(loopStartTime: Double) {
+        val endTime = loopStartTime + targetFrameTime
+
+        while (glfwGetTime() < endTime) {
+            try {
+                Thread.sleep(1)
+            } catch (ie: InterruptedException) {
+            }
+        }
+    }
+
     /**
      * Setup and run the game engine its main loop.
+     *
+     * This call blocks.
      */
     fun run() {
         try {
             init()
-            loop()
-            // Destroy window
+            mainLoop()
             glfwDestroyWindow(window!!)
-            keyCallback?.free()
         } finally {
-            // Terminate GLFW
-            glfwTerminate()
+            keyCallback?.free()
             errorCallback?.free()
+            glfwTerminate()
         }
     }
 
