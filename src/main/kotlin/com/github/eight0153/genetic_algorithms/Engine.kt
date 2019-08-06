9f6103eba3a3ba46b13d5eb6aa2e19a04f218ce0
Anthony Dickson <eight0153@gmail.com>
2019-08-06 19:34:34 +1200

    implement basic mouse input handler and give the info text a bit of an upgrade

--- src/main/kotlin/com/github/eight0153/genetic_algorithms/Engine.kt
+++ src/main/kotlin/com/github/eight0153/genetic_algorithms/Engine.kt
@@ -5,12 +5,13 @@ import org.lwjgl.glfw.GLFWErrorCallback
 import org.lwjgl.opengl.GL
 import org.lwjgl.opengl.GL33.*
 import org.lwjgl.system.MemoryUtil.NULL
+import kotlin.random.Random
 
 data class Colour(val red: Float = 0f, val green: Float = 0f, val blue: Float = 0f, val alpha: Float = 1f)
 data class Size(val width: Int = 0, val height: Int = 0)
 
 class Engine(
-    windowName: String,
+    private val windowName: String,
     /** The width and height of the window in pixels. */
     windowSize: Size = Size(
         800,
@@ -23,7 +24,8 @@ class Engine(
     /** An error callback that will print GLFW error messages to System.err. */
     private val errorCallback: GLFWErrorCallback? = glfwSetErrorCallback(GLFWErrorCallback.createPrint(System.err))
     private val window: Long
-    private val keyboardInput: KeyboardInputHandler
+    private val keyboard: KeyboardInputHandler
+    private val mouse: MouseInputHandler
     private val renderer: Renderer
     private val frameRateLogger = FrameRateLogger()
 
@@ -70,7 +72,8 @@ class Engine(
         GL.createCapabilities()
 
         renderer = Renderer()
-        keyboardInput = KeyboardInputHandler(window)
+        keyboard = KeyboardInputHandler(window)
+        mouse = MouseInputHandler(window)
     }
 
     private fun mainLoop() {
@@ -102,34 +105,62 @@ class Engine(
 
     }
 
-    private fun printControls(lineWidth: Int = 80) {
-        val header = "Controls"
-        val headerPadding = " ".repeat((lineWidth - header.length - 2) / 2)
+    private fun printInfo(lineWidth: Int = 80) {
+        val titlePadding = " ".repeat((lineWidth - windowName.length - 2) / 2)
 
         println("#${"=".repeat(lineWidth - 2)}#")
 
-        // Draw box around the header.
-        println("$headerPadding+${"-".repeat(header.length)}+")
-        println("$headerPadding|$header|")
-        println("$headerPadding+${"-".repeat(header.length)}+")
+        // Draw box around the title.
+        println("$titlePadding+${"-".repeat(windowName.length)}+")
+        println("$titlePadding|$windowName|")
+        println("$titlePadding+${"-".repeat(windowName.length)}+")
 
-        println("F1: Toggle frame rate logging.")
+        // Print the info text and center it.
+        val infoMessages = arrayOf(
+            "This just renders a triangle at this point.",
+            "Nothing too interesting.",
+            "I know, the title is bit of a let down, isn't it?",
+            "But at least there is this sort of fancy text formatting, right??"
+        )
+
+        for (message in infoMessages) {
+            println(" ".repeat((lineWidth - message.length) / 2) + message)
+        }
+        println()
+
+        // Print the controls
+        val header = "Controls"
+        println(header)
+        println("-".repeat(header.length))
+
+        println("F1: \tToggle frame rate logging.")
+        println(
+            "LMD: \tChange background colour rapidly while LMD is held down. \n" +
+                    "\t\tWARNING: This feature may potentially trigger seizures for people with \n" +
+                    "\t\tphotosensitive epilepsy. \n" +
+                    "\t\tUser discretion is advised."
+        )
         println("#${"=".repeat(lineWidth - 2)}#")
     }
 
     private fun handleInput() {
         // This must be called before events are polled in order to correctly store the previous state.
-        keyboardInput.update()
-        // TODO: Handle mouse input
+        keyboard.update()
+        mouse.update()
 
-        // Poll for window events. The key callback will only be invoked during this call.
+        // Poll for window events. The key and mouse callbacks will only be invoked during this call.
         glfwPollEvents()
 
 
-        if (keyboardInput.wasReleased(GLFW_KEY_ESCAPE)) {
+        if (keyboard.wasReleased(GLFW_KEY_ESCAPE)) {
             glfwSetWindowShouldClose(window, true)
-        } else if (keyboardInput.wasPressed(GLFW_KEY_F1)) {
+        } else if (keyboard.wasPressed(GLFW_KEY_F1)) {
             frameRateLogger.toggle()
+        }
+
+        // EPILEPSY WARNING: Flashing Colours
+        if (mouse.isHeldDown(GLFW_MOUSE_BUTTON_LEFT)) {
+            glClearColor(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1.0f)
         }
     }
 
@@ -162,7 +193,8 @@ class Engine(
 
     private fun cleanup() {
         renderer.cleanup()
-        keyboardInput.cleanup()
+        keyboard.cleanup()
+        mouse.cleanup()
         errorCallback?.free()
         glfwDestroyWindow(window)
         glfwTerminate()
@@ -175,7 +207,7 @@ class Engine(
      */
     fun run() {
         try {
-            printControls()
+            printInfo()
             mainLoop()
         } finally {
             cleanup()
---
+++ src/main/kotlin/com/github/eight0153/genetic_algorithms/MouseInputHandler.kt
@@ -0,0 +1,108 @@
+package com.github.eight0153.genetic_algorithms
+
+import org.joml.Vector2d
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.glfw.GLFWCursorEnterCallback
+import org.lwjgl.glfw.GLFWCursorPosCallback
+import org.lwjgl.glfw.GLFWMouseButtonCallback
+import org.lwjgl.glfw.GLFWScrollCallback
+
+class MouseInputHandler(window: Long) {
+    /** Maps mouse buttons to true if the button is down, false otherwise. */
+    private val buttonState = Array(GLFW_MOUSE_BUTTON_LAST) { false }
+    /** Maps mouse buttons to true if the button was down, false otherwise. */
+    private val previousButtonState = Array(GLFW_MOUSE_BUTTON_LAST) { false }
+
+    val position = Vector2d()
+    private val previousPosition = Vector2d()
+    /** How much the mouse moved between the current and previous frame. */
+    val deltaPosition = Vector2d()
+
+    val scrollOffset = Vector2d()
+
+    private val buttonCallback: GLFWMouseButtonCallback?
+    private val cursorEnterCallback: GLFWCursorEnterCallback?
+    private val scrollCallback: GLFWScrollCallback?
+    private val positionCallback: GLFWCursorPosCallback?
+
+    init {
+        buttonCallback = glfwSetMouseButtonCallback(window, object : GLFWMouseButtonCallback() {
+            override fun invoke(window: Long, button: Int, action: Int, mods: Int) {
+                when (action) {
+                    GLFW_PRESS -> buttonState[button] = true
+                    GLFW_RELEASE -> buttonState[button] = false
+                }
+            }
+        })
+
+        cursorEnterCallback = glfwSetCursorEnterCallback(window, object : GLFWCursorEnterCallback() {
+            override fun invoke(window: Long, entered: Boolean) {
+                if (entered) {
+                    // Reset the previous and current positions to avoid erroneous jumps in mouse location
+                    // TODO: Is this even an actual problem? Might be better off just not dealing with this.
+                    previousPosition.set(position)
+                    deltaPosition.zero()
+                }
+            }
+        })
+
+        scrollCallback = glfwSetScrollCallback(window, object : GLFWScrollCallback() {
+            override fun invoke(window: Long, xoffset: Double, yoffset: Double) {
+                scrollOffset.set(xoffset, yoffset)
+            }
+        })
+
+        positionCallback = glfwSetCursorPosCallback(window, object : GLFWCursorPosCallback() {
+            override fun invoke(window: Long, xpos: Double, ypos: Double) {
+                position.set(xpos, ypos)
+                previousPosition.sub(position, deltaPosition)
+            }
+        })
+    }
+
+    fun update() {
+        buttonState.copyInto(previousButtonState)
+        previousPosition.set(position)
+        // Reset delta position to prevent the delta being non-zero even if the mouse wasn't moved recently.
+        deltaPosition.zero()
+        scrollOffset.zero()
+    }
+
+    /** Check if [button] is down. */
+    fun isDown(button: Int): Boolean {
+        return buttonState[button]
+    }
+
+    /** Check if [button] is up. */
+    fun isUp(button: Int): Boolean {
+        return !isDown(button)
+    }
+
+    /** Check if [button] is being held down (was down for the current and previous frames). */
+    fun isHeldDown(button: Int): Boolean {
+        return isDown(button) && previousButtonState[button]
+    }
+
+    /** Check if [button] is being held up (was up for the current and previous frames). */
+    fun isHeldUp(button: Int): Boolean {
+        return isUp(button) && !previousButtonState[button]
+    }
+
+
+    /** Check if [button] is was pressed in the current frame. */
+    fun wasPressed(button: Int): Boolean {
+        return isDown(button) && !previousButtonState[button]
+    }
+
+
+    /** Check if [button] is was released in the current frame. */
+    fun wasReleased(button: Int): Boolean {
+        return isUp(button) && previousButtonState[button]
+    }
+
+    fun cleanup() {
+        buttonCallback?.free()
+        scrollCallback?.free()
+        positionCallback?.free()
+    }
+}
