dfcd889780a9c3a2443cb0a335c6484193035af4
Anthony Dickson <eight0153@gmail.com>
2019-08-08 15:43:34 +1200

    add controls for rotating, panning and zooming the camera

--- src/main/kotlin/com/github/eight0153/genetic_algorithms/game/GameManager.kt
+++ src/main/kotlin/com/github/eight0153/genetic_algorithms/game/GameManager.kt
@@ -5,6 +5,7 @@ import com.github.eight0153.genetic_algorithms.engine.graphics.Mesh
 import com.github.eight0153.genetic_algorithms.engine.input.KeyboardInputHandler
 import com.github.eight0153.genetic_algorithms.engine.input.MouseInputHandler
 import org.lwjgl.glfw.GLFW
+import org.lwjgl.opengl.GL11.GL_LINES
 import org.lwjgl.opengl.GL33
 import kotlin.random.Random
 
@@ -16,59 +17,74 @@ class GameManager : GameManagerI {
 
     private lateinit var camera: Camera
     private val cameraMoveSpeed = 1f
+    private val cameraRotateSpeed = 10f
+    private val cameraZoomSpeed = 10f
 
     override fun init(windowSize: Size, windowName: String) {
         camera = Camera(windowSize)
+        camera.translate(z = -5f)
 
-        val quad = GameObject(
+        val cube = GameObject(
             Mesh(
                 arrayOf(
-                    -0.5f, -0.5f, 0.0f,
-                    -0.5f, 0.5f, 0.0f,
-                    0.5f, -0.5f, 0.0f,
-                    0.5f, 0.5f, 0.0f
+                    -0.5f, -0.5f, 0.5f,
+                    -0.5f, 0.5f, 0.5f,
+                    0.5f, -0.5f, 0.5f,
+                    0.5f, 0.5f, 0.5f,
+                    -0.5f, -0.5f, -0.5f,
+                    -0.5f, 0.5f, -0.5f,
+                    0.5f, -0.5f, -0.5f,
+                    0.5f, 0.5f, -0.5f
                 ),
                 arrayOf(
                     0.5f, 0.0f, 0.0f,
                     0.0f, 0.5f, 0.0f,
                     0.0f, 0.0f, 0.5f,
+                    0.0f, 0.5f, 0.5f,
+                    0.5f, 0.0f, 0.0f,
+                    0.0f, 0.5f, 0.0f,
+                    0.0f, 0.0f, 0.5f,
                     0.0f, 0.5f, 0.5f
                 ),
-                arrayOf(0, 1, 2, 1, 2, 3)
+                arrayOf(
+                    0, 1, 2, 1, 2, 3, // Front face
+                    0, 1, 4, 1, 4, 5, // Left face
+                    2, 3, 6, 3, 6, 7, // Right face
+                    1, 3, 5, 3, 5, 7, // Top face
+                    0, 2, 4, 2, 4, 6, // Bottom face
+                    4, 5, 6, 5, 6, 7  // Back face
+                )
             )
         )
 
-        quad.transform.translate(z = -1.0f)
-        gameObjects = arrayOf(quad)
-//        cubeMesh = Mesh(
-//            arrayOf(
-//                -0.5f, -0.5f, -0.5f,
-//                -0.5f, 0.5f, -0.5f,
-//                0.5f, -0.5f, -0.5f,
-//                0.5f, 0.5f, -0.5f
-//                -0.5f, -0.5f, -1.5f,
-//                -0.5f, 0.5f, -1.5f,
-//                0.5f, -0.5f, -1.5f,
-//                0.5f, 0.5f, -1.5f
-//            ),
-//            arrayOf(
-//                0.5f, 0.0f, 0.0f,
-//                0.0f, 0.5f, 0.0f,
-//                0.0f, 0.0f, 0.5f,
-//                0.0f, 0.5f, 0.5f,
-//                0.5f, 0.0f, 0.0f,
-//                0.0f, 0.5f, 0.0f,
-//                0.0f, 0.0f, 0.5f,
-//                0.0f, 0.5f, 0.5f
-//            ),
-//            arrayOf(0, 1, 2, 1, 2, 3, // Front face
-//                    0, 1, 4, 1, 4, 5, // Left face
-//                    2, 3, 6, 3 , 6, 7, // Right face
-//                    1, 3, 5, 3, 5, 7, // Top face
-//                    0, 2, 4, 2, 4, 6, // Bottom face
-//                    4, 5, 6, 5, 6, 7  // Back face
-//                )
-//        )
+        val axes = GameObject(
+            Mesh(
+                // Duplicate origin point so that colour is uniform for the entire line and we get a:
+                // - red line for the x-axis
+                // - green line for the y-axis
+                // - blue line for the z-axis
+                arrayOf(
+                    0.0f, 0.0f, 0.0f,
+                    100.0f, 0.0f, 0.0f,
+                    0.0f, 0.0f, 0.0f,
+                    0.0f, 100.0f, 0.0f,
+                    0.0f, 0.0f, 0.0f,
+                    0.0f, 0.0f, 100.0f
+                ),
+                arrayOf(
+                    0.5f, 0.0f, 0.0f,
+                    0.5f, 0.0f, 0.0f,
+                    0.0f, 0.5f, 0.0f,
+                    0.0f, 0.5f, 0.0f,
+                    0.0f, 0.0f, 0.5f,
+                    0.0f, 0.0f, 0.5f
+                ),
+                arrayOf(0, 1, 2, 3, 4, 5),
+                GL_LINES
+            )
+        )
+
+        gameObjects = arrayOf(cube, axes)
         renderer = Renderer(camera)
         printInfo(windowName)
     }
@@ -78,6 +94,12 @@ class GameManager : GameManagerI {
             return false
         } else if (keyboard.wasPressed(GLFW.GLFW_KEY_F1)) {
             frameRateLogger.toggle()
+        } else if (keyboard.wasPressed(GLFW.GLFW_KEY_F2)) {
+            gameObjects[1].shouldRender = !gameObjects[1].shouldRender
+        } else if (keyboard.wasPressed(GLFW.GLFW_KEY_F3)) {
+            camera.position.zero()
+            camera.translate(z = -5f)
+            camera.rotation.identity()
         }
 
         // EPILEPSY WARNING: Flashing Colours
@@ -85,18 +107,43 @@ class GameManager : GameManagerI {
             GL33.glClearColor(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1.0f)
         }
 
+        // Translation on the camera pose along the x and y axes needs to be flipped for objects to appear to move
+        // 'naturally'.
         if (keyboard.isDown(GLFW.GLFW_KEY_W)) {
-            camera.translate(y = cameraMoveSpeed * delta.toFloat())
-        } else if (keyboard.isDown(GLFW.GLFW_KEY_S)) {
             camera.translate(y = -cameraMoveSpeed * delta.toFloat())
+        } else if (keyboard.isDown(GLFW.GLFW_KEY_S)) {
+            camera.translate(y = cameraMoveSpeed * delta.toFloat())
         }
 
         if (keyboard.isDown(GLFW.GLFW_KEY_A)) {
-            camera.translate(x = -cameraMoveSpeed * delta.toFloat())
-        } else if (keyboard.isDown(GLFW.GLFW_KEY_D)) {
             camera.translate(x = cameraMoveSpeed * delta.toFloat())
+        } else if (keyboard.isDown(GLFW.GLFW_KEY_D)) {
+            camera.translate(x = -cameraMoveSpeed * delta.toFloat())
         }
 
+        if (mouse.isHeldDown(GLFW.GLFW_MOUSE_BUTTON_RIGHT)) {
+            camera.rotate(
+                // Moving the mouse left/right orbits anti-clockwise/clockwise around the y-axis.
+                x = mouse.deltaPosition.y * delta.toFloat() * cameraRotateSpeed,
+                // Moving the mouse up/down orbits about the x-axis.
+                y = mouse.deltaPosition.x * delta.toFloat() * cameraRotateSpeed
+            )
+        } else if (mouse.isHeldDown(GLFW.GLFW_MOUSE_BUTTON_MIDDLE)) {
+            camera.translate(
+                x = mouse.deltaPosition.x * delta.toFloat(),
+                y = -mouse.deltaPosition.y * delta.toFloat()
+            )
+        }
+
+        // Camera Zoom
+        camera.translate(z = (mouse.scrollOffset.y * delta * cameraZoomSpeed).toFloat())
+
+
+        if (keyboard.isDown(GLFW.GLFW_KEY_LEFT_CONTROL) && keyboard.wasPressed(GLFW.GLFW_KEY_EQUAL)) { // `Ctrl` + `=`
+            camera.translate(z = delta.toFloat() * cameraZoomSpeed)
+        } else if (keyboard.isDown(GLFW.GLFW_KEY_LEFT_CONTROL) && keyboard.wasPressed(GLFW.GLFW_KEY_MINUS)) { // `Ctrl` + `-`
+            camera.translate(z = -1 * delta.toFloat() * cameraZoomSpeed)
+        }
         return true
     }
 
@@ -132,7 +179,7 @@ class GameManager : GameManagerI {
 
         // Print the info text and center it.
         val infoMessages = arrayOf(
-            "This just renders a square at this point.",
+            "This just renders a cube at this point.",
             "Nothing too interesting.",
             "I know, the title is bit of a let down, isn't it?",
             "But at least there is this sort of fancy text formatting, right??"
@@ -148,17 +195,23 @@ class GameManager : GameManagerI {
         println(header)
         println("-".repeat(header.length))
 
-        println("F1: \tToggle frame rate logging")
-        println(
-            "LMD: \tChange background colour rapidly while LMD is held down. \n" +
-                    "\t\tWARNING: This feature may potentially trigger seizures for people with \n" +
-                    "\t\tphotosensitive epilepsy. \n" +
-                    "\t\tUser discretion is advised."
+        val spacingFormat = "%-15s %s"
+
+        val controls = mapOf<String, String>(
+            Pair("F1:", "Toggle frame rate logging"),
+            Pair("F2:", "Toggle XYZ axes"),
+            Pair("F3:", "Reset the camera"),
+            Pair("MMB:", "Pan the camera"),
+            Pair("RMB:", "Rotate the camera"),
+            Pair("Mouse Wheel:", "Zoom the camera"),
+            Pair("`Ctrl` + `-`:", "Zoom the camera out"),
+            Pair("`Ctrl` + `+`:", "Zoom the camera in"),
+            Pair("W/A/S/D:", "Pan the camera up/left/down/right")
         )
-        println("W: Move up")
-        println("S: Move down")
-        println("A: Move left")
-        println("D: Move right")
+
+        for ((key, function) in controls) {
+            println(spacingFormat.format(key, function))
+        }
         println("#${"=".repeat(lineWidth - 2)}#")
     }
 }
