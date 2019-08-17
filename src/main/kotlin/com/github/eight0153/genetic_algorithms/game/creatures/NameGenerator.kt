09a477b3f6c9fa2438d7312d8ff0712963a8e887
Anthony Dickson <eight0153@gmail.com>
2019-08-18 08:39:57 +1200

    add random name generator

---
+++ src/main/kotlin/com/github/eight0153/genetic_algorithms/game/creatures/NameGenerator.kt
@@ -0,0 +1,56 @@
+package com.github.eight0153.genetic_algorithms.game.creatures
+
+import com.github.eight0153.genetic_algorithms.engine.Utils
+
+/** Generates names of creature species in a similar style to the Ubuntu release code names. */
+object NameGenerator {
+    private val adjectives: MutableMap<Char, MutableSet<String>> = HashMap()
+    private val nouns: MutableMap<Char, MutableSet<String>> = HashMap()
+
+    init {
+        val adjectivesFile = Utils.loadResource("naming/adjectives.txt")
+        parseFileContents(adjectivesFile, adjectives)
+
+        val nounsFile = Utils.loadResource("/naming/nouns.txt")
+        parseFileContents(nounsFile, nouns)
+    }
+
+    /**
+     * Read the contents of a file and index by first character.
+     *
+     * The file may contains lines that start with:
+     * - A hash '#' that indicates that the line is a comment.
+     * - A '[' which indicates that the line is indicating which section (block of words starting with the same letter)
+     * we are in.
+     *
+     * The results are stored in [destination].
+     *
+     * This function is idempotent.
+     */
+    private fun parseFileContents(fileContents: String, destination: MutableMap<Char, MutableSet<String>>) {
+        loop@ for (line in fileContents.lines()) {
+            when (line.first()) {
+                // Skip comments marked by hash
+                '#' -> continue@loop
+                '[' -> continue@loop
+                else -> {
+                    var processedLine = line.trim()
+                    processedLine = processedLine.split(" ").joinToString(" ") { it.capitalize() }
+                    processedLine = processedLine.split("-").joinToString("-") { it.capitalize() }
+
+                    destination.getOrPut(processedLine[0].toLowerCase(), { HashSet() }).add(processedLine)
+                }
+            }
+        }
+    }
+
+    /** Generate a random name. */
+    fun random(): String {
+        val startingLetter = adjectives.keys.random()
+
+        val adjective = adjectives[startingLetter]!!.random()
+        val noun = nouns[startingLetter]!!.random()
+
+        return "$adjective $noun"
+    }
+}
