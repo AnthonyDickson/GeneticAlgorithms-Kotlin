2a48eed10ab80a3380c7193a7b990fc250569810
Anthony Dickson <eight0153@gmail.com>
2019-08-19 22:19:48 +1200

    restructure database and allow for multiple runs in same schema

--- src/main/kotlin/com/github/eight0153/genetic_algorithms/game/creatures/CensusDataStore.kt
+++ src/main/kotlin/com/github/eight0153/genetic_algorithms/game/creatures/CensusDataStore.kt
@@ -34,6 +34,8 @@ class CensusDataStore(
     private var connection: Connection? = null
     private var shouldQuit: Boolean = false
 
+    private var runId: Int = -1
+
     private var batchProcessingCoroutine: Job? = null
     private val speciesBuffer = ArrayList<Species>()
     private val creaturesBuffer = ArrayList<Creature>()
@@ -52,96 +54,9 @@ class CensusDataStore(
 
         val statement = connection?.createStatement()
 
-        // TODO: Have something akin to a run id to distinguish between objects created by different instances of the
-        //  app and stop recreating the entire database each time the app is run.
-        var sql = "DROP DATABASE IF EXISTS genetic_algorithms"
-        statement?.execute(sql)
-
         //language=MySQL
-        sql = "CREATE DATABASE genetic_algorithms"
-        statement?.execute(sql)
-
-        //language=MySQL
-        sql = """
-            CREATE TABLE IF NOT EXISTS `genetic_algorithms`.`censuses`(
-              `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-              PRIMARY KEY (`id`))
-            """.trimIndent()
-        statement?.execute(sql)
-
-        //language=MySQL
-        sql = """
-            CREATE TABLE IF NOT EXISTS `genetic_algorithms`.`chromosomes` (
-              `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-              `replication_chance` DOUBLE NOT NULL,
-              `death_chance` DOUBLE NOT NULL,
-              `speed` DOUBLE NOT NULL,
-              `size` DOUBLE NOT NULL,
-              `colour_red` DOUBLE NOT NULL,
-              `colour_green` DOUBLE NOT NULL,
-              `colour_blue` DOUBLE NOT NULL,
-              `metabolic_efficiency` DOUBLE NOT NULL,
-              `sensory_range` DOUBLE NOT NULL,
-              `greediness` DOUBLE NOT NULL,
-              `thriftiness` DOUBLE NOT NULL,
-              `shininess` DOUBLE NOT NULL,
-              PRIMARY KEY (`id`),
-              UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
-        """.trimIndent()
-        statement?.execute(sql)
-
-        //language=MySQL
-        sql = """
-            CREATE TABLE IF NOT EXISTS `genetic_algorithms`.`species` (
-              `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-              `name` VARCHAR(45) NOT NULL,
-              PRIMARY KEY (`id`),
-              UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-              UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
-        """.trimIndent()
-        statement?.execute(sql)
-
-        //language=MySQL
-        sql = """
-            CREATE TABLE IF NOT EXISTS `genetic_algorithms`.`creatures` (
-              `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-              `age` INT(10) UNSIGNED NOT NULL,
-              `chromosome_id` INT(10) UNSIGNED NOT NULL,
-              `species_id` INT(10) UNSIGNED NOT NULL,
-              UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-              INDEX `species_id_idx` (`species_id` ASC) VISIBLE,
-              INDEX `chromosome_id_idx` (`chromosome_id` ASC) VISIBLE,
-              CONSTRAINT `chromosome_fk`
-                FOREIGN KEY (`chromosome_id`)
-                REFERENCES `genetic_algorithms`.`chromosomes` (`id`)
-                ON UPDATE CASCADE 
-                ON DELETE CASCADE,
-              CONSTRAINT `species_fk`
-                FOREIGN KEY (`species_id`)
-                REFERENCES `genetic_algorithms`.`species` (`id`)
-                ON UPDATE CASCADE 
-                ON DELETE CASCADE)
-        """.trimIndent()
-        statement?.execute(sql)
-
-        //language=MySQL
-        sql = """
-            CREATE TABLE IF NOT EXISTS `genetic_algorithms`.`census_participants` (
-              `census_id` INT(10) UNSIGNED NOT NULL,
-              `creature_id` INT(10) UNSIGNED NOT NULL,
-              INDEX `census_fk_idx` (`census_id` ASC) VISIBLE,
-              INDEX `creature_id_idx` (`creature_id` ASC) VISIBLE,
-              PRIMARY KEY (`creature_id`, `census_id`),
-              CONSTRAINT `census_fk`
-                FOREIGN KEY (`census_id`)
-                REFERENCES `genetic_algorithms`.`censuses` (`id`)
-                ON UPDATE CASCADE 
-                ON DELETE CASCADE,
-              CONSTRAINT `creature_fk`
-                FOREIGN KEY (`creature_id`)
-                REFERENCES `genetic_algorithms`.`creatures` (`id`)
-                ON UPDATE CASCADE 
-                ON DELETE CASCADE)
+        var sql = """
+            CREATE SCHEMA IF NOT EXISTS `genetic_algorithms` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci
         """.trimIndent()
         statement?.execute(sql)
 
@@ -150,6 +65,152 @@ class CensusDataStore(
             USE genetic_algorithms
         """.trimIndent()
         statement?.execute(sql)
+
+        //language=MySQL
+        sql = """
+            CREATE TABLE IF NOT EXISTS `genetic_algorithms`.`runs`
+            (
+                `id`          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                `create_time` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY (`id`),
+                UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+            )
+                ENGINE = InnoDB
+            """.trimIndent()
+        statement?.execute(sql)
+
+        //language=MySQL
+        sql = """
+            CREATE TABLE IF NOT EXISTS `genetic_algorithms`.`censuses`
+            (
+                `id`           INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+                `run_id`       INT UNSIGNED     NOT NULL,
+                `date_created` DATETIME         NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY (`id`, `run_id`),
+                INDEX `fk_censuses_runs1_idx` (`run_id` ASC) VISIBLE,
+                CONSTRAINT `fk_censuses_runs1`
+                    FOREIGN KEY (`run_id`)
+                        REFERENCES `genetic_algorithms`.`runs` (`id`)
+                        ON DELETE NO ACTION
+                        ON UPDATE NO ACTION
+            )
+                ENGINE = InnoDB
+                AUTO_INCREMENT = 4
+                DEFAULT CHARACTER SET = utf8mb4
+                COLLATE = utf8mb4_0900_ai_ci
+        """.trimIndent()
+        statement?.execute(sql)
+
+        //language=MySQL
+        sql = """
+            CREATE TABLE IF NOT EXISTS `genetic_algorithms`.`species`
+            (
+                `id`     INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+                `run_id` INT UNSIGNED     NOT NULL,
+                `name`   VARCHAR(45)      NOT NULL,
+                PRIMARY KEY (`id`, `run_id`),
+                INDEX `fk_species_runs1_idx` (`run_id` ASC) VISIBLE,
+                CONSTRAINT `fk_species_runs1`
+                    FOREIGN KEY (`run_id`)
+                        REFERENCES `genetic_algorithms`.`runs` (`id`)
+                        ON DELETE NO ACTION
+                        ON UPDATE NO ACTION
+            )
+                ENGINE = InnoDB
+                AUTO_INCREMENT = 620
+                DEFAULT CHARACTER SET = utf8mb4
+                COLLATE = utf8mb4_0900_ai_ci
+        """.trimIndent()
+        statement?.execute(sql)
+
+        //language=MySQL
+        sql = """
+            CREATE TABLE IF NOT EXISTS `genetic_algorithms`.`creatures`
+            (
+                `id`                   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+                `run_id`               INT UNSIGNED     NOT NULL,
+                `species_id`           INT(10) UNSIGNED NOT NULL,
+                `age`                  INT(10) UNSIGNED NOT NULL,
+                `replication_chance`   DOUBLE           NOT NULL,
+                `death_chance`         DOUBLE           NOT NULL,
+                `speed`                DOUBLE           NOT NULL,
+                `size`                 DOUBLE           NOT NULL,
+                `colour_red`           DOUBLE           NOT NULL,
+                `colour_green`         DOUBLE           NOT NULL,
+                `colour_blue`          DOUBLE           NOT NULL,
+                `metabolic_efficiency` DOUBLE           NOT NULL,
+                `sensory_range`        DOUBLE           NOT NULL,
+                `greediness`           DOUBLE           NOT NULL,
+                `thriftiness`          DOUBLE           NOT NULL,
+                `shininess`            DOUBLE           NOT NULL,
+                PRIMARY KEY (`id`, `run_id`),
+                INDEX `fk_creatures_runs1_idx` (`run_id` ASC) VISIBLE,
+                INDEX `fk_creatures_species1_idx` (`species_id` ASC, `run_id` ASC) VISIBLE,
+                CONSTRAINT `fk_creatures_runs1`
+                    FOREIGN KEY (`run_id`)
+                        REFERENCES `genetic_algorithms`.`runs` (`id`)
+                        ON DELETE NO ACTION
+                        ON UPDATE NO ACTION,
+                CONSTRAINT `fk_creatures_species1`
+                    FOREIGN KEY (`species_id`, `run_id`)
+                        REFERENCES `genetic_algorithms`.`species` (`id`, `run_id`)
+                        ON DELETE NO ACTION
+                        ON UPDATE NO ACTION
+            )
+                ENGINE = InnoDB
+                AUTO_INCREMENT = 2965
+                DEFAULT CHARACTER SET = utf8mb4
+                COLLATE = utf8mb4_0900_ai_ci
+        """.trimIndent()
+        statement?.execute(sql)
+
+        //language=MySQL
+        sql = """
+            CREATE TABLE IF NOT EXISTS `genetic_algorithms`.`census_participants`
+        (
+            `run_id`       INT UNSIGNED     NOT NULL,
+            `census_id`  INT(10) UNSIGNED NOT NULL,
+            `creature_id` INT(10) UNSIGNED NOT NULL,
+            PRIMARY KEY (`run_id`, `census_id`, `creature_id`),
+            INDEX `fk_census_participants_runs1_idx` (`run_id` ASC) VISIBLE,
+            INDEX `fk_census_participants_creatures1_idx` (`creature_id` ASC, `run_id` ASC) VISIBLE,
+            CONSTRAINT `fk_census_participants_runs1`
+                FOREIGN KEY (`run_id`)
+                    REFERENCES `genetic_algorithms`.`runs` (`id`)
+                    ON DELETE NO ACTION
+                    ON UPDATE NO ACTION,
+            CONSTRAINT `fk_census_participants_censuses1`
+                FOREIGN KEY (`run_id`, `census_id`)
+                    REFERENCES `genetic_algorithms`.`censuses` (`run_id`, `id`)
+                    ON DELETE NO ACTION
+                    ON UPDATE NO ACTION,
+            CONSTRAINT `fk_census_participants_creatures1`
+                FOREIGN KEY (`creature_id`, `run_id`)
+                    REFERENCES `genetic_algorithms`.`creatures` (`id`, `run_id`)
+                    ON DELETE NO ACTION
+                    ON UPDATE NO ACTION
+        )
+            ENGINE = InnoDB
+            DEFAULT CHARACTER SET = utf8mb4
+            COLLATE = utf8mb4_0900_ai_ci
+        """.trimIndent()
+        statement?.execute(sql)
+
+        sql = """
+            INSERT INTO `genetic_algorithms`.`runs` VALUES ()
+        """.trimIndent()
+        statement?.execute(sql)
+
+        sql = """
+            SELECT LAST_INSERT_ID()
+        """.trimIndent()
+        val resultSet = statement?.executeQuery(sql)
+
+        if (resultSet?.first() == true) {
+            runId = resultSet.getInt(1)
+        }
+
+        resultSet?.close()
 
         statement?.closeOnCompletion()
 
@@ -177,15 +238,15 @@ class CensusDataStore(
                 // are being built (this could cause creatures to be added whose species was not yet added to the
                 // database, causing foreign key constrain issues).
                 val speciesBatch = java.util.ArrayList<Species>(speciesBuffer.size)
-                speciesBatch.addAll(speciesBuffer)
-                speciesBuffer.clear()
-
                 val creaturesBatch = java.util.ArrayList<Creature>(creaturesBuffer.size)
-                creaturesBatch.addAll(creaturesBuffer)
-                creaturesBuffer.clear()
-
                 val censusesBatch = java.util.ArrayList<Census>(censusesBuffer.size)
+
+                speciesBatch.addAll(speciesBuffer)
+                creaturesBatch.addAll(creaturesBuffer)
                 censusesBatch.addAll(censusesBuffer)
+
+                speciesBuffer.clear()
+                creaturesBuffer.clear()
                 censusesBuffer.clear()
 
                 insertSpecies(speciesBatch)
@@ -200,12 +261,13 @@ class CensusDataStore(
     private fun insertSpecies(species: List<Species>) {
         //language=MySQL
         val sql = """
-            INSERT INTO `species` (name) VALUES (?) 
+            INSERT INTO `species` (id, run_id, name) VALUES (?, $runId, ?) 
         """.trimIndent()
         val statement = connection?.prepareStatement(sql)
 
         for (theSpecies in species) {
-            statement?.setString(1, theSpecies.name)
+            statement?.setInt(1, theSpecies.id)
+            statement?.setString(2, theSpecies.name)
             statement?.addBatch()
         }
 
@@ -215,47 +277,33 @@ class CensusDataStore(
 
     private fun insertCreatures(creatures: List<Creature>) {
         //language=MySQL
-        var sql = """
-            INSERT INTO `chromosomes` 
-              (id, replication_chance, death_chance, speed, size, colour_red, 
-                colour_blue, colour_green, metabolic_efficiency, sensory_range, 
-                greediness, thriftiness, shininess) 
-              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) 
-        """.trimIndent()
-        val insertChromosomeStatement = connection?.prepareStatement(sql)
-
-        //language=MySQL
-        sql = """
-            INSERT INTO `creatures` (id, age, chromosome_id, species_id) 
-              VALUES (?, ?, ?, ?)
+        val sql = """
+            INSERT INTO `creatures` (id, run_id, species_id, age, 
+            replication_chance, death_chance, speed, size, 
+            colour_red, colour_green, colour_blue, metabolic_efficiency, 
+            sensory_range, greediness, thriftiness, shininess) 
+              VALUES ( ?, $runId,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         """.trimIndent()
         val insertCreatureStatement = connection?.prepareStatement(sql)
 
         for (creature in creatures) {
-            insertChromosomeStatement?.setInt(1, creature.chromosome.id)
-            insertChromosomeStatement?.setDouble(2, creature.chromosome[REPLICATION_CHANCE])
-            insertChromosomeStatement?.setDouble(3, creature.chromosome[DEATH_CHANCE])
-            insertChromosomeStatement?.setDouble(4, creature.chromosome[SPEED])
-            insertChromosomeStatement?.setDouble(5, creature.chromosome[SIZE])
-            insertChromosomeStatement?.setDouble(6, creature.chromosome[COLOUR_RED])
-            insertChromosomeStatement?.setDouble(7, creature.chromosome[COLOUR_BLUE])
-            insertChromosomeStatement?.setDouble(8, creature.chromosome[COLOUR_GREEN])
-            insertChromosomeStatement?.setDouble(9, creature.chromosome[METABOLIC_EFFICIENCY])
-            insertChromosomeStatement?.setDouble(10, creature.chromosome[SENSORY_RANGE])
-            insertChromosomeStatement?.setDouble(11, creature.chromosome[GREEDINESS])
-            insertChromosomeStatement?.setDouble(12, creature.chromosome[THRIFTINESS])
-            insertChromosomeStatement?.setDouble(13, creature.chromosome[SHININESS])
-            insertChromosomeStatement?.addBatch()
-
             insertCreatureStatement?.setInt(1, creature.id)
-            insertCreatureStatement?.setInt(2, creature.age)
-            insertCreatureStatement?.setInt(3, creature.chromosome.id)
-            insertCreatureStatement?.setInt(4, creature.species!!.id)
+            insertCreatureStatement?.setInt(2, creature.species!!.id)
+            insertCreatureStatement?.setInt(3, creature.age)
+            insertCreatureStatement?.setDouble(4, creature.chromosome[REPLICATION_CHANCE])
+            insertCreatureStatement?.setDouble(5, creature.chromosome[DEATH_CHANCE])
+            insertCreatureStatement?.setDouble(6, creature.chromosome[SPEED])
+            insertCreatureStatement?.setDouble(7, creature.chromosome[SIZE])
+            insertCreatureStatement?.setDouble(8, creature.chromosome[COLOUR_RED])
+            insertCreatureStatement?.setDouble(9, creature.chromosome[COLOUR_BLUE])
+            insertCreatureStatement?.setDouble(10, creature.chromosome[COLOUR_GREEN])
+            insertCreatureStatement?.setDouble(11, creature.chromosome[METABOLIC_EFFICIENCY])
+            insertCreatureStatement?.setDouble(12, creature.chromosome[SENSORY_RANGE])
+            insertCreatureStatement?.setDouble(13, creature.chromosome[GREEDINESS])
+            insertCreatureStatement?.setDouble(14, creature.chromosome[THRIFTINESS])
+            insertCreatureStatement?.setDouble(15, creature.chromosome[SHININESS])
             insertCreatureStatement?.addBatch()
         }
-
-        insertChromosomeStatement?.executeBatch()
-        insertChromosomeStatement?.closeOnCompletion()
 
         insertCreatureStatement?.executeBatch()
         insertCreatureStatement?.closeOnCompletion()
@@ -264,19 +312,19 @@ class CensusDataStore(
     private fun insertCensuses(censuses: List<Census>) {
         //language=MySQL
         var sql: String = """
-                INSERT INTO censuses (id) VALUES (?)
+                INSERT INTO censuses (id, run_id) VALUES (?, $runId)
             """.trimIndent()
         val insertCensusStatement = connection?.prepareStatement(sql)
 
         //language=MySQL
         sql = """
-            UPDATE `creatures` SET age = ? WHERE id = ?
+            UPDATE `creatures` SET age = ? WHERE id = ? AND run_id = $runId
         """.trimIndent()
         val updateCreatureStatement = connection?.prepareStatement(sql)
 
         //language=MySQL
         sql = """
-            INSERT INTO census_participants (census_id, creature_id) VALUES (?, ?)
+            INSERT INTO census_participants (census_id, creature_id, run_id) VALUES (?, ?, $runId)
         """.trimIndent()
         val insertCensusParticipantStatement = connection?.prepareStatement(sql)
 
