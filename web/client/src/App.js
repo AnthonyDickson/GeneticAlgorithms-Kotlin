2100d28b0b43e7f8f68f24e456fd0c73ed64cef1
Anthony Dickson <eight0153@gmail.com>
2019-08-20 20:49:52 +1200

    start using Ant Design for react components and styling

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,8 +1,7 @@
-import React, {Component} from 'react';
-
-import logo from './logo.svg';
-
 import './App.css';
+
+import React, {Component} from 'react';
+import {Breadcrumb, Button, Form, Icon, Input, Layout, Menu, Typography} from 'antd';
 
 class App extends Component {
     state = {
@@ -40,36 +39,75 @@ class App extends Component {
     };
 
     render() {
+        // TODO: Use React Router for breadcrumbs. See: https://ant.design/components/breadcrumb/
+        const routes = [
+            {
+                path: 'index',
+                breadcrumbName: 'First-level Menu',
+            },
+            {
+                path: 'first',
+                breadcrumbName: 'Second-level Menu',
+            },
+            {
+                path: 'second',
+                breadcrumbName: 'Third-level Menu',
+            },
+        ];
+
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-                <p>{this.state.response}</p>
-                <form onSubmit={this.handleSubmit}>
-                    <p>
-                        <strong>Post to Server:</strong>
-                    </p>
-                    <input
-                        type="text"
-                        value={this.state.post}
-                        onChange={e => this.setState({post: e.target.value})}
-                    />
-                    <button type="submit">Submit</button>
-                </form>
-                <p>{this.state.responseToPost}</p>
-            </div>
+            <Layout>
+                <Layout.Sider
+                    style={{
+                        overflow: 'auto',
+                        height: '100vh',
+                        position: 'fixed',
+                        left: 0,
+                    }}
+                >
+                    <div className="logo"/>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Icon type="ordered-list"/>
+                            <span className="nav-text">Runs</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="bar-chart"/>
+                            <span className="nav-text">Default Dashboard</span>
+                        </Menu.Item>
+                    </Menu>
+                </Layout.Sider>
+                <Layout style={{marginLeft: 200}}>
+                    <Layout.Header style={{background: '#fff', padding: '24px 16px', minHeight: 120}}>
+                        <Typography>
+                            <Typography.Title>Title</Typography.Title>
+                            <Breadcrumb routes={routes}/>
+                        </Typography>
+                    </Layout.Header>
+                    <Layout.Content style={{margin: '24px 16px 0'}}>
+                        <div style={{padding: 24, background: '#fff', minHeight: 360}}>
+                            <p>{this.state.response}</p>
+                            <Form onSubmit={this.handleSubmit} className="server-message-form">
+                                <Form.Item>
+                                    <strong>Post to Server:</strong>
+                                    <Input
+                                        type="text"
+                                        placeholder="Message"
+                                        value={this.state.post}
+                                        onChange={e => this.setState({post: e.target.value})}
+                                    />
+                                </Form.Item>
+
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit">Submit</Button>
+                                </Form.Item>
+                            </Form>
+                            <p>{this.state.responseToPost}</p>
+                        </div>
+                    </Layout.Content>
+                    <Layout.Footer style={{textAlign: 'center'}}>Ant Design ©2018 Created by Ant UED</Layout.Footer>
+                </Layout>
+            </Layout>
         );
     }
 }
